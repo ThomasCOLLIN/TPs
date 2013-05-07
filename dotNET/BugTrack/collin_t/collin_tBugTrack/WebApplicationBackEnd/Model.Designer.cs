7001fb5712d47b81a19abed7e2b0930203c88d87
Thomas COLLIN <thmcollin@gmail.com>
2013-05-07 23:18:12 +0200

    TP dotNET BugTrack Back end

---
+++ dotNET/BugTrack/collin_t/collin_tBugTrack/WebApplicationBackEnd/Model.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La génération de code par défaut est désactivée pour le modèle 'E:\Documents\GitHub\TPs\dotNET\BugTrack\collin_t\collin_tBugTrack\WebApplicationBackEnd\Model.edmx'. 
+// Pour l'activer, modifiez la valeur du concepteur 'Stratégie de génération de code'.
+// Cette propriété est disponible dans la fenêtre Propriétés, lorsque le modèle est ouvert
+// dans le concepteur.
